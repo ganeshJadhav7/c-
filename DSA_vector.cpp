4837fcd3d63db98b63a32244fc76932491009619
GaneshJadhav <jadhavgana9975@gmail.com>
2025-02-23 12:56:16 +0530

    .

--- DSA_vector.cpp
+++ DSA_vector.cpp
@@ -1,10 +1,11 @@
-3#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> v;
+    // size and capacity of vector
+    /*vector<int> v;
     cout<<"size : "<<v.size()<<endl;
     cout<<"capacity : "<<v.capacity()<<endl;
 
@@ -20,7 +21,66 @@ int main()
     cout<<"size : "<<v.size()<<endl;
     cout<<"capacity : "<<v.capacity()<<endl;
 
-    v.push_back(4);
+    v.resize(5);
     cout<<"size : "<<v.size()<<endl;
     cout<<"capacity : "<<v.capacity()<<endl;
+
+    v.resize(10);
+    cout<<"size : "<<v.size()<<endl;
+    cout<<"capacity : "<<v.capacity()<<endl;
+
+    v.pop_back();
+    v.pop_back();
+    cout<<"size : "<<v.size()<<endl;
+    cout<<"capacity : "<<v.capacity()<<endl;*/
+
+
+
+    // for loop
+    // Taking input and giving output from dynamic size of array.
+    /*vector<int> v;
+    for(int i=0; i<5; i++)
+    {
+        int ele;
+        cout<<"Enter ele : ";
+        cin>>ele;
+        v.push_back(ele);
+    }
+    cout<<"Elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }*/
+
+
+    // Taking input and giving output from fixed size of array.
+    /*vector<int> v(5);
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    cout<<"Elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }*/
+
+
+    // for each loop
+    /*vector<int> v(5);
+
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    cout<<"elements : ";
+    for(int ele:v)
+    {
+        cout<<ele<<" ";
+    }
+    cout<<endl;*/
+
+    
 }
