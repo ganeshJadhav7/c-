3770076ec9eeca63f69d25a380ce85e1686c5b9e
GaneshJadhav <jadhavgana9975@gmail.com>
2025-02-25 13:38:30 +0530

    added some codes

--- DSA_vector.cpp
+++ DSA_vector.cpp
@@ -83,4 +83,112 @@ int main()
     cout<<endl;*/
 
     
+    // while loop
+    /*vector<int> v(5);
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    cout<<"elements : ";
+    int idx = 0;
+    while(idx<v.size())
+    {
+        cout<<v[idx++]<<" ";
+    }*/
+
+
+    // Find the last occurance of x in given array.
+    /*vector<int> v(7);
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    int x, ans;
+    cout<<"Enter a elemet to find it,s last occurance : ";
+    cin>>x;
+    for(int i=v.size()-1; i>0; i--)
+    {
+        if(v[i] == x)
+        {
+            cout<<"Last occurance of given element is : "<<i;
+            break;
+        }
+    }*/
+
+
+
+    // Count the number of occurances of a particular element x.
+    /*vector<int> v(7);
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    int x, count=0;
+    cout<<"Enter a elemet to find it,s occurance count : ";
+    cin>>x;
+    for(int ele:v)
+    {
+        if(ele == x)
+        {
+            count++;
+        }
+    }
+    cout<<"occurance count of given element : "<<count;*/
+
+
+
+    // Count the numer of elements strictly greater than x.
+    /*vector<int> v(7);
+    cout<<"Enter elements : ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    int x, count=0;
+    cout<<"Enter x : ";
+    cin>>x;
+    for(int ele:v)
+    {
+        if(ele > x)
+        {
+            count++;
+        }
+    }
+    cout<<"Count is : "<<count;*/
+
+
+    // check the given array is sorted or not.
+    /*int array[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(array) / sizeof(array[0]);
+    bool sorted = true;
+    for(int i=1; i<size-1; i++)
+    {
+        if(array[i]<=array[i-1])
+        {
+            sorted = false;
+        }
+    }
+    cout<<sorted;*/
+
+    // Find the diffrence between sum of ele at even indices and sum of ele at odd indices.
+    /*int array[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(array) / sizeof(array[0]);
+    int evensum=0, oddsum=0;
+    for(int i=0; i<size; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            evensum  = evensum + array[i];
+        }
+        else
+        {
+            oddsum += array[i];
+        }
+    }
+    cout<<evensum<<endl;
+    cout<<oddsum<<endl;
+    cout<<"Diffrence b/w evensum and oddsum is : "<<evensum - oddsum;*/
 }
